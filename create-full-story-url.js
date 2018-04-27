82ceb531521c561a684282d54670bc2de2169226
Stephen Prockow <sprockow@gmail.com>
2018-04-26 22:54:57 -0700

    add gitignore. fixed issue

--- create-full-story-url.js
+++ create-full-story-url.js
@@ -18,6 +18,6 @@ const quotedAndCommaDelimitted = accountRecords.reduce((acc, accountRecord) => {
 
 const urlFormatted = encodeURIComponent(`[${quotedAndCommaDelimitted}]`);
 
-const url = `https://app.fullstory.com/ui/1G7V8/segments/everyone/people:search:(:((user_accountId_str:==${urlFormatted})):():():():)/0`;
+const url = `https://app.fullstory.com/ui/1G7V8/segments/everyone/people:search:(:((user_accountId_str:==:${urlFormatted})):():():():)/0`;
 
 console.log(url);
